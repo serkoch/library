b9f6639c40f3461d07d25ede21bfcfb77d28cec0
nevermind <nevermind359@gmail.com>
2024-12-13 12:04:37 -0500

    Add logic for library population via dialog form

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-// const cardAuthor = document.querySelector('.card-author');
-// const cardTitle = document.querySelector('.card-title');
-// const cardPages = document.querySelector('.card-pages');
-// const cardRead = document.querySelector('.card-read');
-const bookshelf = document.querySelector('.library');
+const bookshelf = document.querySelector('.bookshelf');
+
 const myLibrary = [
   { title: 'Divine Comedy', author: 'Dante Alighieri', pages: 350, read: false },
   { title: 'Harry Potter', author: 'J.K. Rowling', pages: 300, read: true },
@@ -15,52 +12,68 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
-function displayBook() {
-  myLibrary.forEach(book => {
-    const cardDiv = document.createElement('div');
-    const cardTitle = document.createElement('p');
-    cardTitle.innerText = book.title;
-    const cardAuthor = document.createElement('p');
-    cardAuthor.innerText = `by ${book.author}`;
-    const cardPages = document.createElement('p');
-    cardPages.innerText = `${book.pages} pages`;
-    const cardRead = document.createElement('input');
-    cardRead.type = 'checkbox';
-    if (book.read) {
-      cardRead.checked = true;
-    }
-    cardDiv.appendChild(cardTitle);
-    cardDiv.appendChild(cardAuthor);
-    cardDiv.appendChild(cardPages);
-    cardDiv.appendChild(cardRead);
-    bookshelf.appendChild(cardDiv);
-  })
+function createBookCard(book) {
+  const cardDiv = document.createElement('div');
+  const cardTitle = document.createElement('p');
+  cardTitle.innerText = book.title;
+  const cardAuthor = document.createElement('p');
+  cardAuthor.innerText = `by ${book.author}`;
+  const cardPages = document.createElement('p');
+  cardPages.innerText = `${book.pages} pages`;
+  const cardRead = document.createElement('input');
+  cardRead.type = 'checkbox';
+  if (book.read) {
+    cardRead.checked = true;
+  }
+  cardDiv.appendChild(cardTitle);
+  cardDiv.appendChild(cardAuthor);
+  cardDiv.appendChild(cardPages);
+  cardDiv.appendChild(cardRead);
+  bookshelf.appendChild(cardDiv);
+  cardDiv.setAttribute('data-num', `${myLibrary.indexOf(book)}`);
 }
 
+function initialLib() {
+  myLibrary.forEach((book) => {
+    createBookCard(book);
+  });
+}
 
-displayBook();
+function addToLib() {
+  createBookCard(myLibrary[myLibrary.length - 1]);
+}
 
-const showButton = document.getElementById("showDialog");
-const favDialog = document.getElementById("favDialog");
-const outputBox = document.querySelector("output");
-const title = favDialog.querySelector("title");
-const confirmBtn = favDialog.querySelector("#confirmBtn");
+initialLib();
 
-// "Show the dialog" button opens the <dialog> modally
-showButton.addEventListener("click", () => {
+const showButton = document.getElementById('showDialog');
+const favDialog = document.getElementById('favDialog');
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+const pages = document.querySelector('#pages');
+const read = document.querySelector('#read');
+const confirmBtn = favDialog.querySelector('#confirmBtn');
+
+showButton.addEventListener('click', () => {
   favDialog.showModal();
 });
 
-// "Cancel" button closes the dialog without submitting because of [formmethod="dialog"], triggering a close event.
-favDialog.addEventListener("close", (e) => {
-  outputBox.value =
-    favDialog.returnValue === "default"
-      ? "No return value."
-      : `ReturnValue: ${favDialog.returnValue}.`; // Have to check for "default" rather than empty string
+favDialog.addEventListener('close', (e) => {
+  e.preventDefault();
+  favDialog.close();
 });
 
-// Prevent the "confirm" button from the default behavior of submitting the form, and close the dialog with the `close()` method, which triggers the "close" event.
-confirmBtn.addEventListener("click", (event) => {
-  event.preventDefault(); // We don't want to submit this fake form
-  favDialog.close(title.value); // Have to send the select box value here.
+function clearForm() {
+  title.value = '';
+  author.value = '';
+  pages.value = '';
+  read.checked = false;
+}
+
+confirmBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  const addBook = new Book(title.value, author.value, pages.value, read.checked);
+  myLibrary.push(addBook);
+  addToLib();
+  favDialog.close();
+  clearForm();
 });
